76269da99439b893741369eadf74c499f1669f8e
NatsuPow <147069989+NatsuPow@users.noreply.github.com>
2024-05-26 20:50:00 +0200

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,21 +1,26 @@
 package fr.ul.miage;
-
-
 import java.io.*;
 import java.net.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
 public class Client {
     private static final String SERVER_ADDRESS = "localhost";
-    private static final int SERVER_PORT = 12349;
-    private static final String SERVER_PASSWORD = "azerty"; // Le mot de passe du serveur est défini ici
+    private static final int SERVER_PORT = 12351;
+    private static int difficulty;
+    private static long nonceStart;
+    private static long nonceIncrement;
+    private static String payload;
+    private static boolean ready = false;
+    private static Long currentNonce;
 
     public static void main(String[] args) {
         try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
              BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
              PrintWriter writer = new PrintWriter(socket.getOutputStream(), true)) {
 
-            Scanner scanner = new Scanner(System.in); // Créer un Scanner pour la saisie du client
+            Scanner scanner = new Scanner(System.in);
 
             // Lecture et traitement des instructions du serveur
             String instruction;
@@ -29,10 +34,10 @@ public class Client {
                         System.out.print("Votre réponse : ");
                         String reponse = scanner.nextLine();
 
-                        // Envoyer la réponse au serveur
+                        // Envoie la réponse au serveur
                         writer.println(reponse);
 
-                        // Vérifier si la réponse est correcte
+                        // Vérifie si la réponse est correcte
                         if (!reponse.equalsIgnoreCase("ITS_ME")) {
                             System.out.println("Mauvaise réponse, déconnexion du serveur...");
                             socket.close();
@@ -49,16 +54,66 @@ public class Client {
                         break;
                     case "HELLO_YOU":
                         // Le client indique qu'il est prêt
-                        writer.println("READY");
+                        System.out.print("Votre réponse : ");
+                        String response = scanner.nextLine();
+                        writer.println(response);
                         break;
                     case "YOU_DONT_FOOL_ME":
                         System.out.println("Mot de passe incorrect. Déconnexion...");
-                        socket.close(); // Déconnecter le client
-                        return; // Sortir de la méthode main pour arrêter le client
+                        socket.close();
+                        return;
                     case "OK":
+                    	ready = true;
                         System.out.println("Connecté et prêt pour des tâches.");
-                        // Le client est connecté et prêt pour d'autres commandes
-                        // Ajoutez ici la gestion des autres commandes du protocole...
+                        // Ajouter une boucle while pour continuer à écouter les instructions du serveur
+                        while ((instruction = reader.readLine()) != null) {
+                        	Thread t = null;
+                            // Traitement de l'instruction
+                            if (instruction.startsWith("NONCE ")) {
+                                String[] parts = instruction.split(" ");
+                                nonceStart = Long.parseLong(parts[1]);
+                                nonceIncrement = Long.parseLong(parts[2]);
+                                System.out.println("NONCE " + nonceStart + " " + nonceIncrement);
+                            }
+
+                            if (instruction.startsWith("SOLVE ")) {
+                                String[] parts = instruction.split(" ");
+                                difficulty = Integer.parseInt(parts[1]); // Stocker la difficulté reçue
+                                System.out.println("SOLVE " + difficulty);
+
+                             // Résoudre la tâche en utilisant la difficulté et les nonces
+                                
+                                if (reader.ready()) {
+                                    payload = reader.readLine().substring(8);
+                                    System.out.println("PAYLOAD " + payload);
+                                }
+                                System.out.println("Le minage commence");
+                                t = new Thread(() -> searchAndValidateNonces(payload, difficulty, nonceStart, nonceIncrement, socket));
+                                t.start();
+                                if(instruction.startsWith("CANCELLED")) {
+                                    t.interrupt(); // suspendre la tâche en cours
+                                    writer.println("READY");
+                                    ready = true;
+                                }
+                            }
+                            if(instruction.startsWith("PROGRESS")) {
+                                if(ready == true) {
+                                    writer.println("NOPE");
+                                }
+                                else {
+                                    // si thread en cours d'exécution
+                                    if(t != null && t.isAlive()) {
+                                        // recup nonce courant 
+                                        long currentNonce = 0; // il faut qu'on trouve comment recup le nonce en cours
+                                        //nonce en hexa
+                                        writer.println("TESTING " + Long.toHexString(currentNonce));
+                                    }
+                                    else {
+                                        writer.println("TESTING");
+                                    }
+                                }
+                            }
+                        }
                         break;
                     default:
                         System.out.println("Instruction inconnue : " + instruction);
@@ -77,9 +132,9 @@ public class Client {
             System.err.println("Erreur de communication : " + e.getMessage());
             e.printStackTrace();
         }
-        
-        }
-    
+    }
+
+    // Fonctions pour le minage
     public static String generateTarget(int difficulty) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < difficulty; i++) {
@@ -88,8 +143,30 @@ public class Client {
         return sb.toString();
       }
 
+      public static String calculateHash(String data, long nonce) {
+        try {
+          MessageDigest digest = MessageDigest.getInstance("SHA-256");
 
-        private static byte[] longToTrimmedBytes(long x) {
+          // Convertir le nonce en bytes sans les zéros de début
+          byte[] nonceBytes = longToTrimmedBytes(nonce);
+
+          // Concaténer les données en string avec les bytes du nonce
+          byte[] dataBytes = data.getBytes("utf-8");
+          byte[] payload = new byte[dataBytes.length + nonceBytes.length];
+          System.arraycopy(dataBytes, 0, payload, 0, dataBytes.length);
+          System.arraycopy(nonceBytes, 0, payload, dataBytes.length, nonceBytes.length);
+
+          // Calculer le hash
+          byte[] hashBytes = digest.digest(payload);
+          return bytesToHex(hashBytes);
+
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+          e.printStackTrace();
+          return null;
+        }
+      }
+
+      private static byte[] longToTrimmedBytes(long x) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         boolean nonZeroFound = false;
 
@@ -99,10 +176,10 @@ public class Client {
             baos.write(b);
             nonZeroFound = true;
           }
-            }
+        }
 
-            return baos.toByteArray();
-            }
+        return baos.toByteArray();
+      }
 
       private static String bytesToHex(byte[] bytes) {
         StringBuilder sb = new StringBuilder();
@@ -111,8 +188,34 @@ public class Client {
         }
         return sb.toString();
       }
-    
-    
+
+      
+      public static void searchAndValidateNonces(String data, int difficulty, long nonceStart, long nonceIncrement, Socket socket) {
+   	    	long nonce = nonceStart;
+    	    String target = generateTarget(difficulty);
+
+    	    while (true) {
+    	        String hash = calculateHash(data, nonce);
+
+    	        if (hash.startsWith(target)) {
+    	            System.out.println("Bon hash trouvé : " + hash + " avec le nonce : " + Long.toHexString(nonce));
+    	            try {
+    	                // Envoyer un message au Serveur de la forme "FOUND " avec premier argument le hash trouvé" "et deuxième argument le nonce trouvé"
+    	                String message = "FOUND " + hash + " " + Long.toHexString(nonce) + difficulty;
+    	                PrintWriter out = new PrintWriter(socket.getOutputStream(), true); // utiliser la variable socket dans la classe Client
+    	                out.println(message);
+
+    	                
+    	            } catch (IOException e) {
+    	                e.printStackTrace();
+    	            }
+    	        }
+
+    	        nonce += nonceIncrement;
+    	    }
+    	}
+
+
+      
+      
 }
-    
-    
